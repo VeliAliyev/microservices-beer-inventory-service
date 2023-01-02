61fb55118838624476a11a2c3f5800fe739336e2
VeliAliyev <veli_aliyev@outlook.com>
2023-01-02 16:06:10 +0300

    initial commit

---
+++ src/main/java/com/velialiyev/microservicesbeerinventoryservice/MicroservicesBeerInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.velialiyev.microservicesbeerinventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesBeerInventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesBeerInventoryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/velialiyev/microservicesbeerinventoryservice/MicroservicesBeerInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.velialiyev.microservicesbeerinventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesBeerInventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
